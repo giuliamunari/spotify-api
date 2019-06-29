355fcd8f752e723c7a738401a31a52d4f7ed119c
Astrid88 <giu.munari@gmail.com>
2019-06-29 15:22:57 +0200

    partial autentication

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+require('./db')
+
+const JWT = require('./auth/jwt')
+const authRouter = require('./auth/router')
+const userRouter = require('./user/router')
+
 const app = express()
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
+app.use(authRouter);
+app.use(userRouter)
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => `Listening on port ${port}`)

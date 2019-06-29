5945db20e86b22ceb08a444dc63955211b32bb64
Astrid88 <giu.munari@gmail.com>
2019-06-29 16:05:19 +0200

    complete authentication

--- auth/router.js
+++ auth/router.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 const { toJWT } = require('./jwt');
 const User = require('../user/model')
 const bcrypt = require('bcrypt');
-//const auth = require('./middleware')
+const auth = require('./middleware')
 
 const router = new Router()
 
@@ -56,27 +56,10 @@ router.post('/tokens', (req, res, next) => {
     }
 })
 
-router.get('/secret-endpoint', (req, res) => {
-    const auth = req.headers.authorization && req.headers.authorization.split(' ')
-    if (auth && auth[0] === 'Bearer' && auth[1]) {
-        try {
-            const data = toData(auth[1])
-            res.send({
-                message: 'Thanks for visiting the secret endpoint.',
-                data
-            })
-        }
-        catch (error) {
-            res.status(400).send({
-                message: `Error ${error.name}: ${error.message}`,
-            })
-        }
-    }
-    else {
-        res.status(401).send({
-            message: 'Please supply some valid credentials'
-        })
-    }
-})
+rrouter.get('/secret-endpoint', auth, (req, res) => {
+    res.send({
+      message: `Thanks for visiting the secret endpoint ${req.user.email}.`,
+    })
+  })
 
 module.exports = router
